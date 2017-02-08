ff202ca58b351df662a8be38204273c4b7bcf39b
CHEN Mingyi <imcmy@outlook.com>
2017-02-08 21:49:58 +0800

    Add archlinuxcn repo
    Add archlinuxcn repo

---
+++ deployer/script/archlinuxcn.sh
@@ -0,0 +1,21 @@
+#/bin/dash
+source=rsync://repo.archlinuxcn.org
+target=/data/mirrors/archlinuxcn
+lock=/tmp/archlinuxcn
+
+[ -f $lock ] && echo "already running" >&2 && exit 0
+touch $lock
+
+fatal() {
+  echo "$1"
+  exit 1
+}
+
+warn() {
+  echo "$1"
+}
+rsync -avzH --timeout=600 --ipv4 \
+      --delay-updates  --delete-delay \
+      ${source} ${target}
+
+rm -f $lock
